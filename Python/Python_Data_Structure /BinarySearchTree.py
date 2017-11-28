226ca55110210fc1f3476c35c7702bebb671495a
MithileshCParab <parab.mithilesh09@gmail.com>
2017-11-28 16:11:36 +0530

    Update Binary_Search_Tree.py via upload

--- Python/Python_Data_Structure /BinarySearchTree.py
+++ Python/Python_Data_Structure /BinarySearchTree.py
@@ -3,6 +3,7 @@ class node:
 		self.value = value
 		self.left_child = None
 		self.right_child = None
+		self.parent = None # pointer to the parent node in tree 
 
 
 class binary_search_tree:
@@ -19,11 +20,13 @@ class binary_search_tree:
 		if value<cur_node.value:
 			if cur_node.left_child == None:
 				cur_node.left_child = node(value)
+				cur_node.left_child.parent=cur_node
 			else:
 				self._insert(value,cur_node.left_child)
 		elif value>cur_node.value:
 			if cur_node.right_child ==None:
 				cur_node.right_child=node(value)
+				cur_node.right_child.parent=cur_node
 			else:
 				self._insert(value, cur_node.right_child)
 		else:
@@ -50,6 +53,87 @@ class binary_search_tree:
 		left_height=self._height(cur_node.left_child,cur_height+1)
 		right_height=self._height(cur_node.right_child,cur_height+1)
 		return max(left_height, right_height)
+
+	# return the node with specified input value 
+	def find(self,value):
+		if self.root!=None:
+			return self._find(value,self.root)
+		else:
+			return None
+	
+	def _find(self, value, cur_node):
+		if value == cur_node.value:
+			return cur_node
+		elif value<cur_node.value and cur_node.left_child!=None:
+			return self._find(value, cur_node.left_child)
+		elif value>cur_node.value and cur_node.right_child!=None:
+			return self._find(value,cur_node.right_child)
+
+
+	def delete_value(self,value):
+		return self.delete_node(self.find(value))
+
+	def delete_node(self, node):
+		#return the node with min value in tree rooted at input node 
+		def min_value_node(n):
+			current = n
+			while current.left_child!=None:
+				current=current.left_child
+			return current
+		#return the number of childred for the specified node 
+		def num_children(n):
+			num_children=0
+			if n.left_child!=None: num_children+=1
+			if n.right_child!=None: num_children+=1
+			return num_children
+
+		# get the parent of the  node to be deleted 
+		node_parent=node.parent
+		#get the number of children of the node to be deleted 
+		node_children = num_children(node)
+
+		# break operation into different cases based on the 
+		# structure of the tree & node to be deleted 
+
+		# CAse 1 (node has no children)
+		if node_children==0:
+			#remove reference to the node from the parent 
+			if node_parent.left_child==node:
+				node_parent.left_child=None
+			else:
+				node_parent.right_child=None
+		# CASE 2 (node has a single child)
+		if node_children==1:
+			# get the single child node 
+			if node.left_child!=None:
+				child=node.left_child
+			else:
+				child=node.right_child
+
+			#replace the node to be deleted with ites child 
+			if node_parent.left_child==node:
+				node_parent.left_child=child
+			else:
+				node_parent.right_child=child
+
+			#correct the parent pointer in node 
+			child.parent=node_parent
+
+		# CASE 3 (node has 3 children)
+		if node_children==2:
+
+			#get  the inorder successor of the deleted node 
+			successor=min_value_node(node.right_child)
+
+			#copy the inorder successor's value to the node formely
+			# holding the value we wished to delete 
+			node.value=successor.value
+
+			#delete the inorder successor now that it's value was 
+			# copied into the other node 
+			self.delete_node(successor)
+
+	# return true if the value exists in the tree 
 
 	def search(self, value):
 		if self.root!=None:
@@ -79,13 +163,13 @@ def fill_tree(tree, num_elems=100, max_int=1000):
 tree = binary_search_tree()
 #tree = fill_tree(tree)
 
-tree.insert(0)
-tree.insert(3)
-tree.insert(2)
-tree.insert(7)
+tree.insert(5)
+tree.insert(4)
+tree.insert(6)
 tree.insert(10)
-tree.insert(30)
-tree.insert(20)
+tree.insert(9)
+tree.insert(11)
+
 
 tree.print_tree()
 
@@ -93,4 +177,8 @@ tree.print_tree()
 print "tree height : "+str(tree.height())
 
 print tree.search(10)
-print tree.search(40)
+
+
+tree.delete_value(4)
+tree.print_tree()
+
