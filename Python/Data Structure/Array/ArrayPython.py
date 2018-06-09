e0e90ff066979584269aced02f42b16db54d7105
MithileshCParab <parab.mithilesh09@gmail.com>
2018-06-09 23:24:41 +0530

    Add files via upload: CD
    dd

--- Python/Data Structure/Array/ArrayPython.py
+++ Python/Data Structure/Array/ArrayPython.py
@@ -1,13 +1,13 @@
-import sys # provides getsizeof function 
+import sys  # provides getsizeof function
 
 data = []
-n = 26 
+n = 26
 
-for k in range(n):  # NOTE : must fix choice of n 
-	a = len(data)   # number of elements 
-	b = sys.getsizeof(data) # actual sixe in bytes 
-	print('Length : {0:3d}; size in bytes : {1:4d}'.format(a, b))
-	data.append(None)  # increase length by one 
+for k in range(n):  # NOTE : must fix choice of n
+    a = len(data)   # number of elements
+    b = sys.getsizeof(data)  # actual sixe in bytes
+    print('Length : {0:3d}; size in bytes : {1:4d}'.format(a, b))
+    data.append(None)  # increase length by one
 
 """
 
