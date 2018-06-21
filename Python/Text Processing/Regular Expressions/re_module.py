653f46bb5bf58edd0e2b646201e5fbb3f5be7640
MithileshCParab <parab.mithilesh09@gmail.com>
2018-06-21 22:47:53 +0530

    Add files via upload

--- Python/Text Processing/Regular Expressions/re_module.py
+++ Python/Text Processing/Regular Expressions/re_module.py
@@ -1,6 +1,7 @@
 import re 
 
-text_to_search = '''
+text_to_search = 
+'''
 abcdefghijklmnopqurtuvwxyz
 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 1234567890
@@ -41,3 +42,4 @@ for match in matches:
 	print(match)
 
 # print(text_to_search[1:4])
+
