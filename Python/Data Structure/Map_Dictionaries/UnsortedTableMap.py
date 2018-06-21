c3f739ce88480b6be6928149684414335830bb0a
MithileshCParab <parab.mithilesh09@gmail.com>
2018-06-21 22:48:05 +0530

    Add files via upload

---
+++ Python/Data Structure/Map_Dictionaries/UnsortedTableMap.py
@@ -0,0 +1,40 @@
+class UnsortedTableMap(MapBase):
+	""" Map implementation using an unordered list. """
+    #------------------------------------------------------------------------------------
+	def __init__(self):
+		""" Create an empty map """
+		self._table = []
+	#------------------------------------------------------------------------------------
+	def __getitem__(self, k):
+		""" Return value associated with key k (raise KeyError if not found). """
+		for item in self._table:
+			if k == item._key:
+				return item._value
+		raise KeyError('Key Error : '+repr(k))
+	#------------------------------------------------------------------------------------
+	def __setitem__(self, k, v):
+		""" Assign value v to key k, overwriting existing value if present. """
+		for item in self._table:
+			if k == item._key:
+				item._value = v
+				return 
+		# did not find match for key 
+		self._table.append(self._item(k, v))
+	#------------------------------------------------------------------------------------
+	def __delitem__(self, k):
+		""" remove item associated with key k (raise KeyError if not found). """
+		for j in range(len(self._table)):
+			if k == self._table[j]._key:
+				self._table.pop(j)
+				return
+		raise KeyError('Key Error: '+ repe(k))
+	#------------------------------------------------------------------------------------
+		def __len__(self):
+			""" Return number of items in the map """
+			return len(self._table)
+    #------------------------------------------------------------------------------------
+		def __iter__(self):
+			""" Generate iteration of the map's key """
+			for item in self._table:
+				yield item._key
+    #------------------------------------------------------------------------------------
