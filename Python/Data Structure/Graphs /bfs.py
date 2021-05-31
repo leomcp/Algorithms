ddf286067159672ab1b74652608bc51739e54a58
leomcp <leomcp550@gmail.com>
2021-05-31 14:32:00 +0530

    BFS : single shortest path retrieval update

--- Python/Data Structure/Graphs /bfs.py
+++ Python/Data Structure/Graphs /bfs.py
@@ -1,10 +1,14 @@
+from __future__ import print_function
+from collections import deque
+
 class Vertex:
 
 	def __init__(self, data):
-		self.data = data 
-		self.neighbours = []
+		self.data = data	
+		self.distance = -1
 		self.visited = False 
-		self.distance = 0 
+		self.parent = None
+		self.neighbours = []
 
 	def addNeighbour(self, v):
 		if v not in self.neighbours:
@@ -12,79 +16,134 @@ class Vertex:
 
 class UDGraph:
 
-	def __init__(self):
+	def __init__(self, n, m, edges, s):
+		self.n = n
+		self.m = m
+		self.edges = edges
+		self.s = s
 		self.vertices = {}
+		self.jsf = "" # Journey So Far ....
 
-	def _addVertex(self, v):
-		self.vertices[v] = Vertex(v)
+		self.build()
+		self.printGraph()
+		self.bfs()
 
-	def _addEdge(self, v1, v2):
-		if v1 not in self.vertices:
-			self._addVertex(v1)
-		if v2 not in self.vertices:
-			self._addVertex(v2)
-
-		self.vertices[v1].addNeighbour(v2)
-		self.vertices[v2].addNeighbour(v1)
-
-	def buildGraph(self, edges):
+	def build(self):
+		for n in range(1, self.n+1):
+			self.vertices[n] = Vertex(n)
 		for edge in edges:
 			self._addEdge(edge[0], edge[1])
 
+	def _addEdge(self, u, v):
+		if u in self.vertices and v in self.vertices:
+			self.vertices[u].addNeighbour(v)
+			self.vertices[v].addNeighbour(u)
+
 	def printGraph(self):
+		print("-"*30)
+		print("UDGraph : ")
 		if self.vertices:
 			for v in sorted(self.vertices):
 				print("{} --> {}".format(v, self.vertices[v].neighbours))
 
-	def _bfs(self, start_v):
-		q = []
-		q.append(start_v)
-		self.vertices[start_v].visited = True 
+	def bfs(self):
+		print("-"*30)
+		if s in self.vertices:
+			self._bfs()
+			if self.jsf:
+				print("BFS : {}".format(self.jsf))
 
-		while len(q)>0:
-			head_v = q.pop(0)
+	def _bfs(self):
+		q = deque()
+		q.append(self.s)
+		self.vertices[self.s].distance = 0
+		self.vertices[self.s].visited = True
 
-			for n in self.vertices[head_v].neighbours:
-				if self.vertices[n].visited == False:
-					q.append(n)
-					self.vertices[n].visited = True 
-					self.vertices[n].distance = self.vertices[head_v].distance + 1
+		while len(q) > 0:
+			head_v = q.popleft()
+			self.jsf = self.jsf + " " +  str(head_v)
 
-	def findShortestDistance(self, start_v, end_v):
-		if self.vertices:
-			if start_v not in self.vertices:
-				return 
-			if end_v not in self.vertices:
-				return 
+			for neighbour in self.vertices[head_v].neighbours:
+				if self.vertices[neighbour].visited == False:
+					self.vertices[neighbour].distance = self.vertices[head_v].distance + 1
+					self.vertices[neighbour].visited = True
+					self.vertices[neighbour].parent = head_v
+					q.append(neighbour)
 
-			self._bfs(start_v)
-			print("Shortest Distance between {} and {} : {}".format(start_v, end_v, self.vertices[end_v].distance))
-
+	def shortestPath(self, e):
+		print("-"*30)
+		print("Shortest Path BFS From {} to {} : ".format(self.s, e))
+		if self.s in self.vertices and e in self.vertices:
+			if self.vertices[e].distance == -1:
+				print("Path does not exists... ")
+			else:
+				print("Distance = {}".format(self.vertices[e].distance))
+				print("Path --> ", end=" ")
+				path = []
+				path.append(e)
+				parent = self.vertices[e].parent
+				while parent!= self.s:
+					path.append(parent)
+					parent = self.vertices[parent].parent
+				path.append(self.s)
+				for _ in range(len(path)):
+					print(path.pop(), end=" ")
+				print()
 
 if __name__ == "__main__":
 
+	# Total number of vertices (from 1 to n)
+	n = 12
+	# Total number of edges 
+	m = 11
+	edges = [[1, 2], [1, 3], [2, 3], [2, 4], [3, 5], [5, 6], [5, 7], [7, 8], [4, 8], [7, 9], [9, 10]]
+	s = 1  # start Vertex
+	e = 10 # End Vertex
 
-	edges = ["AB", "BD", "AC", "CD", "DE", "DF"]
+	udg = UDGraph(n, m, edges, s)
 
-	g = UDGraph()
+	udg.shortestPath(10)
+	udg.shortestPath(8)
+	udg.shortestPath(6)
+	udg.shortestPath(11)
 
-	g.buildGraph(edges)
-	g.printGraph()
-
-	g.findShortestDistance("A", "F")
-
+	print("-"*30)
 
 """
+
 OUTPUT : 
 
-
-A --> ['B', 'C']
-B --> ['A', 'D']
-C --> ['A', 'D']
-D --> ['B', 'C', 'E', 'F']
-E --> ['D']
-F --> ['D']
-Shortest Distance between A and F : 3
-
+------------------------------
+UDGraph : 
+1 --> [2, 3]
+2 --> [1, 3, 4]
+3 --> [1, 2, 5]
+4 --> [2, 8]
+5 --> [3, 6, 7]
+6 --> [5]
+7 --> [5, 8, 9]
+8 --> [7, 4]
+9 --> [7, 10]
+10 --> [9]
+11 --> []
+12 --> []
+------------------------------
+BFS :  1 2 3 4 5 8 6 7 9 10
+------------------------------
+Shortest Path BFS From 1 to 10 : 
+Distance = 5
+Path -->  1 3 5 7 9 10 
+------------------------------
+Shortest Path BFS From 1 to 8 : 
+Distance = 3
+Path -->  1 2 4 8 
+------------------------------
+Shortest Path BFS From 1 to 6 : 
+Distance = 3
+Path -->  1 3 5 6 
+------------------------------
+Shortest Path BFS From 1 to 11 : 
+Path does not exists... 
+------------------------------
 """
 
